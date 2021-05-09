00b16a01d617f94556df82ab52c70bce8a726704
mitubaEX <jun-nakamura@c-fo.com>
2021-05-09 18:42:59 +0900

    get filename and origin path

--- lua/blame_open.lua
+++ lua/blame_open.lua
@@ -1,18 +1,39 @@
 local vim = vim
 
-local function greet()
-  local filename = 'lua/blame_open.lua'
-  local line_number = vim.fn.line('.')
-  local handle = io.popen('git blame -l -L ' .. line_number .. ',+1 ' .. filename)
+local function git_origin_path()
+  local handle = io.popen('git config --get remote.origin.url')
   local result = handle:read("*a")
   handle:close()
+
+  -- remove .git string
+  return result:sub(0, -6)
+end
+
+local function greet()
+  local filename = vim.fn.expand('%')
+  -- if empty filename
+  if filename == '' then
+    return
+  end
+
+  local line_number = vim.fn.line('.')
+  local handle = io.popen('git blame -l -L ' .. line_number .. ',+1 ' .. filename)
+  local blame_result = handle:read("*a")
+  handle:close()
+
   local commit_hash = ''
-  for str in string.gmatch(result, "%S+") do
+  for str in string.gmatch(blame_result, "%S+") do
     commit_hash = str
     break
   end
+
+  -- not commit hash
+  if commit_hash == '0000000000000000000000000000000000000000' then
+    return
+  end
+
   -- https://github.com/<author>/<repo_name>/commit/<commit_hash>
-  os.execute('open https://github.com/mitubaEX/blame_open/commit/' .. commit_hash)
+  os.execute('open ' .. git_origin_path() .. '/commit/' .. commit_hash)
 end
 
 return {
